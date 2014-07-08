8d5610d07f61eab775b6a98aaae1e15397cb028b
bisaria <rupabisaria@hotmail.com>
2014-07-08 09:34:42 +0530

    R Script for Plot 2

---
+++ plot2.R
@@ -0,0 +1,15 @@
+# Assignment: Plot 2
+# Goal: to examine how household energy usage varies over a 2-day period in February, 2007
+library(sqldf)
+f <- file("household_power_consumption.txt")
+DF <- sqldf("select * from f where Date == '1/2/2007' or Date == '2/2/2007'",  # select data only for two day period
+            dbname = tempfile(), 
+            file.format = list(header = T, row.names = F, sep=";"))
+df<-DF[,c("Date","Time","Global_active_power")]  # get the relevant columns from the data frame
+df$DateTime<-strptime(paste(df$Date, df$Time),format="%d/%m/%Y %H:%M:%S")
+png(file="plot2.png",width=480,height=480, bg = "transparent")
+plot(df$DateTime, as.numeric( as.character(df$Global_active_power )), 
+     ylab = "Global Active Power (kilowatts)",
+     xlab = "", 
+     type = "l")
+dev.off()
